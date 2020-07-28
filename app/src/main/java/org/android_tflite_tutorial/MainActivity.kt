6445716f03a8386c44b55c4cd105975fd8047ea1
Reuben Morais <reuben.morais@gmail.com>
2020-07-28 11:20:02 +0200

    Check record audio permission and request it on startup

--- app/src/main/java/org/android_tflite_tutorial/MainActivity.kt
+++ app/src/main/java/org/android_tflite_tutorial/MainActivity.kt
@@ -1,14 +1,30 @@
 package org.android_tflite_tutorial
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkRecordAudioPermission()
+    }
+
+    private fun checkRecordAudioPermission() {
+        // Permission is automatically granted on SDK < 23 upon installation.
+        if (Build.VERSION.SDK_INT >= 23) {
+            val permission = Manifest.permission.RECORD_AUDIO
+
+            if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(permission), 3)
+            }
+        }
     }
 
     fun onRecordClick(v: View?) {
