2f9d6e35a1ea08e425b28577c23177eb658f2f60
Reuben Morais <reuben.morais@gmail.com>
2020-07-28 12:00:08 +0200

    Stream audio from microphone and transcribe it

--- app/src/main/java/org/android_tflite_tutorial/MainActivity.kt
+++ app/src/main/java/org/android_tflite_tutorial/MainActivity.kt
@@ -2,6 +2,9 @@ package org.android_tflite_tutorial
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
 import android.os.Build
 import android.os.Bundle
 import android.view.View
@@ -10,10 +13,13 @@ import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import org.mozilla.deepspeech.libdeepspeech.DeepSpeechModel
 import java.io.File
-
+import java.util.concurrent.atomic.AtomicBoolean
 
 class MainActivity : AppCompatActivity() {
     private var model: DeepSpeechModel? = null
+
+    private var transcriptionThread: Thread? = null
+    private var isRecording: AtomicBoolean = AtomicBoolean(false)
 
     private val TFLITE_MODEL_FILENAME = "deepspeech-0.7.4-models.tflite"
     private val SCORER_FILENAME = "deepspeech-0.7.4-models.scorer"
@@ -58,14 +64,67 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    private fun transcriptionThreadImpl() {
+        // We read from the recorder in chunks of 2048 shorts. With a model that expects its input
+        // at 16000Hz, this corresponds to 2048/16000 = 0.128s or 128ms.
+        val audioBufferSize = 2048
+        val audioData = ShortArray(audioBufferSize)
+
+        runOnUiThread { btnStartInference.text = "Stop Recording" }
+
+        model?.let { model ->
+            val streamContext = model.createStream()
+
+            val recorder = AudioRecord(
+                MediaRecorder.AudioSource.VOICE_RECOGNITION,
+                model.sampleRate(),
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                audioBufferSize
+            )
+            recorder.startRecording()
+
+            while (isRecording.get()) {
+                recorder.read(audioData, 0, audioBufferSize)
+                model.feedAudioContent(streamContext, audioData, audioData.size)
+                val decoded = model.intermediateDecode(streamContext)
+                runOnUiThread { transcription.text = decoded }
+            }
+
+            val decoded = model.finishStream(streamContext)
+
+            runOnUiThread {
+                btnStartInference.text = "Start Recording"
+                transcription.text = decoded
+            }
+
+            recorder.stop()
+        }
+    }
+
+    private fun startListening() {
+        if (isRecording.compareAndSet(false, true)) {
+            transcriptionThread = Thread(Runnable { transcriptionThreadImpl() }, "Transcription Thread")
+            transcriptionThread?.start()
+        }
+    }
+
+    private fun stopListening() {
+        isRecording.set(false)
+    }
+
     fun onRecordClick(v: View?) {
         if (model == null) {
             if (!createModel()) {
                 return
             }
             status.append("Created model.\n")
+        }
+
+        if (isRecording.get()) {
+            stopListening()
         } else {
-            status.append("Model already created.\n")
+            startListening()
         }
     }
 
